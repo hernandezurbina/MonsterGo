d785edb33d54896229f68de752b7a1eaec1ce81a
Victor Hernandez-Urbina <hernandezurbina@gmail.com>
2020-04-03 20:07:14 +0100

    Added code and new elements to scene

---
+++ Assets/Scripts/WalkShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkShoot : MonoBehaviour
+{
+    public GameObject pokeball;
+    public float shootingSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            GameObject pokego = Instantiate(pokeball);
+            pokego.transform.position = transform.position;
+            Rigidbody rb = pokego.GetComponent<Rigidbody>();
+            Camera cam = GetComponentInChildren<Camera>();
+            rb.velocity = cam.transform.rotation * Vector3.forward * shootingSpeed;
+        }
+    }
+}
